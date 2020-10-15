5780106ff1c800dceacb638a7dd68916a5020f1c
Lucas O'Connell <lucasoconnell4@gmail.com>
2020-10-15 19:01:03 +0700

    Convert App.js and BusinessList.js
    App.js and BusinessList.js converted to React hooks with the app still functioning as expected.

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -2,15 +2,14 @@ import React from 'react';
 import './BusinessList.css';
 import Business from '../Business/Business.js';
 
-class BusinessList extends React.Component {
-  render() {
-    return (
-      <div className="BusinessList">
-        {this.props.businesses.map(business =>
-          <Business business={business} key={business.id}/>)}
-      </div>
-    );
-  }
+function BusinessList(props) {
+
+  return (
+    <div className="BusinessList">
+      {props.businesses.map(business =>
+        <Business business={business} key={business.id}/>)}
+    </div>
+  );
 }
 
 export default BusinessList;
