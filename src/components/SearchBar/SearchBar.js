2063e42050b3a7226dc631de8f41fa7963abffd7
Lucas O'Connell <lucasoconnell4@gmail.com>
2020-10-16 13:44:38 +0700

    Convert SearchBar.js to hooks
    SearchBar.js converted to React hooks with the app still functioning as expected.

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,94 +1,73 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './SearchBar.css';
 
-class SearchBar extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      term: '',
-      location: '',
-      sortBy: 'best_match'
-    };
+function SearchBar(props) {
+  const [term, setTerm] = useState('');
+  const [location, setLocation] = useState('');
+  const [sortBy, setSortBy] = useState('best_match');
 
-    this.handleTermChange = this.handleTermChange.bind(this);
-    this.handleLocationChange = this.handleLocationChange.bind(this);
-    this.handleSearch = this.handleSearch.bind(this);
-    this.handleSortByChange = this.handleSortByChange.bind(this);
+  const sortByOptions = {
+    'Best Match': 'best_match',
+    'Highest Rated': 'rating',
+    'Most Reviewed': 'review_count'
+  };
 
-    this.sortByOptions = {
-      'Best Match': 'best_match',
-      'Highest Rated': 'rating',
-      'Most Reviewed': 'review_count'
-    };
+  function getSortByClass(sortByOption) {
+    return ((sortBy === sortByOption) ? 'active' : '');
   }
 
-  getSortByClass(sortByOption) {
-    return ((this.state.sortBy === sortByOption) ? 'active' : '');
+  function handleSortByChange(sortByOption) {
+    setSortBy(sortByOption);
   }
 
-  handleSortByChange(sortByOption) {
-    this.setState({
-      sortBy: sortByOption
-    });
+  function handleTermChange(e) {
+    setTerm(e.target.value);
   }
 
-  handleTermChange(e) {
-    this.setState({
-      term: e.target.value
-    });
+  function handleLocationChange(e) {
+    setLocation(e.target.value);
   }
 
-  handleLocationChange(e) {
-    this.setState({
-      location: e.target.value
-    });
-  }
-
-  handleSearch(e) {
-    this.props.searchYelp(
-      this.state.term,
-      this.state.location,
-      this.state.sortBy
-    );
+  function handleSearch(e) {
+    props.searchYelp(term, location, sortBy);
     e.preventDefault();
   }
 
-  renderSortByOptions() {
-    return Object.keys(this.sortByOptions).map(sortByOption => {
-      let sortByOptionValue = this.sortByOptions[sortByOption];
+  function renderSortByOptions() {
+    return Object.keys(sortByOptions).map(sortByOption => {
+      let sortByOptionValue = sortByOptions[sortByOption];
+      console.log(sortByOptionValue);
       return (
       <li
       key={sortByOptionValue}
-      className={this.getSortByClass(sortByOptionValue)}
-      onClick={this.handleSortByChange.bind(this, sortByOptionValue)}
+      className={getSortByClass(sortByOptionValue)}
+      onClick={() => handleSortByChange(sortByOptionValue)}
       >
         {sortByOption}
       </li>);
     });
   }
 
-  render() {
-    return (
-      <div className="SearchBar">
-        <div className="SearchBar-sort-options">
-          <ul>
-            {this.renderSortByOptions()}
-          </ul>
-        </div>
-        <div className="SearchBar-fields">
-          <input
-          placeholder="Search Businesses"
-          onChange={this.handleTermChange} />
-          <input
-          placeholder="Where?"
-          onChange={this.handleLocationChange} />
-        </div>
-        <div className="SearchBar-submit">
-          <a onClick={this.handleSearch}>Let's Go</a>
-        </div>
+  return (
+    <div className="SearchBar">
+      <div className="SearchBar-sort-options">
+        <ul>
+          {renderSortByOptions()}
+        </ul>
       </div>
-    );
-  }
+      <div className="SearchBar-fields">
+        <input
+        placeholder="Search Businesses"
+        onChange={handleTermChange} />
+        <input
+        placeholder="Where?"
+        onChange={handleLocationChange} />
+      </div>
+      <div className="SearchBar-submit">
+        <a onClick={handleSearch}>Let's Go</a>
+      </div>
+    </div>
+  );
 }
 
 export default SearchBar;
