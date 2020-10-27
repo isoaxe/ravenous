781727a91f7f268a98b6598c88b4a171cb557d7a
Lucas O'Connell <lucasoconnell4@gmail.com>
2020-10-27 09:49:22 +0700

    Removed unnecessary props from Slider
    The getAriaValueText prop was not providing a function and was thus removed along with it's associated helper function.

--- src/components/PriceSlider/PriceSlider.js
+++ src/components/PriceSlider/PriceSlider.js
@@ -2,22 +2,6 @@ import React, { useState } from 'react';
 import { Typography, Slider } from '@material-ui/core';
 import './PriceSlider.css';
 
-function valuetext(value) {
-  return `${value}°C`;
-}
-
-function label(index) {
-  if (index === 1) {
-    return '$';
-  } else if (index === 2) {
-    return '$$';
-  } else if (index === 3) {
-    return '$$$';
-  } else {
-    return '$$$$';
-  }
-}
-
 
 function RangeSlider() {
   const [price, setPrice] = useState([1, 4]);
@@ -39,10 +23,21 @@ function RangeSlider() {
         aria-labelledby="range-slider-custom"
         min={1}
         max={4}
-        getAriaValueText={valuetext}
       />
     </div>
   );
+}
+
+function label(index) {
+  if (index === 1) {
+    return '$';
+  } else if (index === 2) {
+    return '$$';
+  } else if (index === 3) {
+    return '$$$';
+  } else {
+    return '$$$$';
+  }
 }
 
 export default RangeSlider;
