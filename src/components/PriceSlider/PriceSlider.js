c172d5bb5b52f8b22c7834e73133e913ae7fcb3d
Lucas O'Connell <lucasoconnell4@gmail.com>
2020-10-27 09:28:22 +0700

    Removed marks from price slider
    Removed the marks underneath the price slider rail. These were rendered redundant after the $ pricing values were added to the thumb in the previous commit. Adjusted the styling to make smaller and better use of space after this change.

--- src/components/PriceSlider/PriceSlider.js
+++ src/components/PriceSlider/PriceSlider.js
@@ -2,25 +2,6 @@ import React, { useState } from 'react';
 import { Typography, Slider } from '@material-ui/core';
 import './PriceSlider.css';
 
-const marks = [
-  {
-    value: 1,
-    label: '$',
-  },
-  {
-    value: 2,
-    label: '$$',
-  },
-  {
-    value: 3,
-    label: '$$$',
-  },
-  {
-    value: 4,
-    label: '$$$$',
-  },
-];
-
 function valuetext(value) {
   return `${value}°C`;
 }
@@ -54,12 +35,11 @@ function RangeSlider() {
         value={price}
         onChange={handleChange}
         valueLabelDisplay="auto"
+        valueLabelFormat={label}
         aria-labelledby="range-slider-custom"
         min={1}
         max={4}
         getAriaValueText={valuetext}
-        valueLabelFormat={label}
-        marks={marks}
       />
     </div>
   );
