0fce2abf1f2f2f67836ff53c74a1b5beec191ec2
Lucas O'Connell <lucasoconnell4@gmail.com>
2020-11-05 20:42:38 +0700

    usePlacesAutocomplete: Correct indentation

--- src/util/usePlacesAutocomplete.js
+++ src/util/usePlacesAutocomplete.js
@@ -38,21 +38,21 @@ const PlacesAutocomplete = (props) => {
     clearSuggestions();
   };
 
-    return (
-      <Combobox onSelect={handleSelect} aria-labelledby="demo">
-        <ComboboxInput id="location" value={value} onChange={handleChange} placeholder="Where?" disabled={!ready} />
-        <ComboboxPopover>
-          <ComboboxList>
-            {status === "OK" &&
-              data.map(({ place_id, structured_formatting: { main_text, secondary_text } }) => (
-                <ComboboxOption key={place_id} value={main_text + ' ' + secondary_text}>
-                  <span>{main_text}</span> <span>{secondary_text}</span>
-                </ComboboxOption>
-              ))}
-          </ComboboxList>
-        </ComboboxPopover>
-      </Combobox>
-    );
-  };
+  return (
+    <Combobox onSelect={handleSelect} aria-labelledby="demo">
+      <ComboboxInput id="location" value={value} onChange={handleChange} placeholder="Where?" disabled={!ready} />
+      <ComboboxPopover>
+        <ComboboxList>
+          {status === "OK" &&
+            data.map(({ place_id, structured_formatting: { main_text, secondary_text } }) => (
+              <ComboboxOption key={place_id} value={main_text + ' ' + secondary_text}>
+                <span>{main_text}</span> <span>{secondary_text}</span>
+              </ComboboxOption>
+            ))}
+        </ComboboxList>
+      </ComboboxPopover>
+    </Combobox>
+  );
+};
 
 export default PlacesAutocomplete;
