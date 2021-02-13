4d61110cf2784fa7ac08d589c8fe24ea67dd00aa
Lucas O'Connell <lucasoconnell4@gmail.com>
2021-02-13 07:44:11 +0700

    searchYelp: Implement private CORS server
    Need to create a private server to replace the previously used public one as it was being discontinued. Server attaches the correct headers to a API request to make it CORS compliant. See separate GitHib repo.
    Issues with hosting the server that on Namecheap. Instead, created a Heroku account and launched the server there.

--- src/util/searchYelp.js
+++ src/util/searchYelp.js
@@ -3,7 +3,7 @@ const apiKey = '7XO3wpS0y_k0hZwjosDddypgOk7wGApu9qgYeMIELYM_rXP-b4M3Vkriy7y5wtzv
 
 
 function searchYelp(term, location, priceString, sortBy) {
-  return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&price=${priceString}&sort_by=${sortBy}`, {headers: {Authorization: `Bearer ${apiKey}`}})
+  return fetch(`https://private-cors-server.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&price=${priceString}&sort_by=${sortBy}`, {headers: {Authorization: `Bearer ${apiKey}`}})
   .then(response => response.json())
   .then(jsonResponse => {
     console.log(jsonResponse);
