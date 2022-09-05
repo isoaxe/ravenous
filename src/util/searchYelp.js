3081bd187095acecb54322c9d740775176615971
Lucas O'Connell <lucasoconnell4@gmail.com>
2022-09-05 21:13:03 +0700

    searchYelp: Extract cors proxy as a variable
    This helps clarify that there are two different urls here - one for the proxy and another for the Yelp api.
    Also shortens the overly long line of code.

--- src/util/searchYelp.js
+++ src/util/searchYelp.js
@@ -2,7 +2,8 @@ const apiKey = '7XO3wpS0y_k0hZwjosDddypgOk7wGApu9qgYeMIELYM_rXP-b4M3Vkriy7y5wtzv
 
 
 function searchYelp(term, location, priceString, sortBy) {
-  return fetch(`https://private-cors-server.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&price=${priceString}&sort_by=${sortBy}`, {headers: {Authorization: `Bearer ${apiKey}`}})
+  const corsProxy = "https://private-cors-server.herokuapp.com/";
+  return fetch(`${corsProxy}https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&price=${priceString}&sort_by=${sortBy}`, {headers: {Authorization: `Bearer ${apiKey}`}})
   .then(response => response.json())
   .then(jsonResponse => {
     console.log(jsonResponse);
