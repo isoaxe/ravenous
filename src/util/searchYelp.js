5581ec581f159aa1850ab432f39e12df2f6f9330
Lucas O'Connell <lucasoconnell4@gmail.com>
2022-09-14 11:10:18 +0700

    searchYelp: Adjust spacing
    + No need for two lines of separation between the api key and function declaration.
    + Best to separate the api request constituents from it's invocation.

--- src/util/searchYelp.js
+++ src/util/searchYelp.js
@@ -1,12 +1,12 @@
 // TODO: Replace the Yelp API key below with one obtained for your own account if hosting remote.
 const apiKey = '7XO3wpS0y_k0hZwjosDddypgOk7wGApu9qgYeMIELYM_rXP-b4M3Vkriy7y5wtzvYMJ_faFlpvahmeQHGEGikl2NHgwMasbhLk9Oe0XBDPHcSSI3_AYdEb-NycfQXnYx';
 
-
 async function searchYelp(term, location, priceString, sortBy) {
   const corsProxy = "https://private-cors-server.herokuapp.com/";
   const yelpApi = "https://api.yelp.com/v3/businesses/search";
   const searchParams = `?term=${term}&location=${location}&price=${priceString}&sort_by=${sortBy}`;
   const fetchOptions = { headers: { Authorization: `Bearer ${apiKey}`}};
+
   const response = await fetch(corsProxy + yelpApi + searchParams, fetchOptions);
   const jsonResponse = await response.json();
   console.log(jsonResponse);
