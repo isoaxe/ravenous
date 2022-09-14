ef59c9b220de87be39442e9d626942bba9fa8573
Lucas O'Connell <lucasoconnell4@gmail.com>
2022-09-14 10:19:55 +0700

    searchYelp: Refactor yelp api call
    Declare the API URL, its parameters and the fetch request options as separate variables. Shortens the line of code and makes it easier to read.

--- src/util/searchYelp.js
+++ src/util/searchYelp.js
@@ -3,7 +3,10 @@ const apiKey = '7XO3wpS0y_k0hZwjosDddypgOk7wGApu9qgYeMIELYM_rXP-b4M3Vkriy7y5wtzv
 
 function searchYelp(term, location, priceString, sortBy) {
   const corsProxy = "https://private-cors-server.herokuapp.com/";
-  return fetch(`${corsProxy}https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&price=${priceString}&sort_by=${sortBy}`, {headers: {Authorization: `Bearer ${apiKey}`}})
+  const yelpApi = "https://api.yelp.com/v3/businesses/search";
+  const searchParams = `?term=${term}&location=${location}&price=${priceString}&sort_by=${sortBy}`;
+  const fetchOptions = { headers: { Authorization: `Bearer ${apiKey}`}};
+  return fetch(corsProxy + yelpApi + searchParams, fetchOptions)
   .then(response => response.json())
   .then(jsonResponse => {
     console.log(jsonResponse);
